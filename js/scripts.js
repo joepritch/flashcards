7d2078a849381664237538c459a2bb1dbbc439ca
Joe Pritchett <joempritchett@gmail.com>
2019-01-15 18:24:16 -0800

    add 8 functioning cards with placeholder text, next step is to give each a unique question and answer, as wells as try to apply text scaling.

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,32 @@ $(document).ready(function() {
     $("#firstquestion").slideToggle();
     $("#firstanswer").slideToggle();
   });
+  $("#secondcard").click(function()  {
+    $("#secondquestion").slideToggle();
+    $("#secondanswer").slideToggle();
+  });
+  $("#thirdcard").click(function()  {
+    $("#thirdquestion").slideToggle();
+    $("#thirdanswer").slideToggle();
+  });
+  $("#fourthcard").click(function()  {
+    $("#fourthquestion").slideToggle();
+    $("#fourthanswer").slideToggle();
+  });
+  $("#fifthcard").click(function()  {
+    $("#fifthquestion").slideToggle();
+    $("#fifthanswer").slideToggle();
+  });
+  $("#sixthcard").click(function()  {
+    $("#sixthquestion").slideToggle();
+    $("#sixthanswer").slideToggle();
+  });
+  $("#seventhcard").click(function()  {
+    $("#seventhquestion").slideToggle();
+    $("#seventhanswer").slideToggle();
+  });
+  $("#eighthcard").click(function()  {
+    $("#eighthquestion").slideToggle();
+    $("#eighthanswer").slideToggle();
+  });
 });
